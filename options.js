554d48895dfaca8e5d4d10a32eafaae7f7e25087
hqweay <hqweay@gmail.com>
2020-09-13 13:40:03 +0800

    add: 用 vue 重构并添加随机获取一条书签的功能

--- options.js
+++ options.js
@@ -1,118 +1,172 @@
 
-$('#btn-click').click(function () {
-    console.log("start...");
-    init();
-})
+new Vue({
+    el: '#app',
+    data: {
+        config: {
+            selected: "random",
+        },
+        folderName: "",
+        recursionConfig: true,
+        randomBookmark: {
+            id: "",
+            url: "",
+            title: "",
+        },
+        folders: [
+            // "Bookmarks bar",
+            "Bookmarks bar/博客",
+            // "other bookmark/picture",
+            // "书签栏/博客",
+            // "书签栏/博客/技术博客"
+        ],
+        markdown: {
+            content: ""
+        },
+        errorInfo: ""
+    },
+    created() {
+        chrome.bookmarks.getTree(this.initConfig);
+    },
+    methods: {
+        init() {
+            chrome.bookmarks.getTree(this.analyseBookMark);
 
 
-function init() {
-    chrome.bookmarks.getTree(analyseBookMark);
-}
+        },
+        // 书签栏 移动设备书签 其它书签
+        analyseBookMark(data) {
+            let value = this.folderName.trim();
+            var words = this.splitBookMarkName(value);
+            data = this.getFirstLevelBookMarksFolder(data);
 
-function splitBookMarkName(value) {
-    // value = "书签栏::博客"
-    var words = value.split('::')
-    return words;
-}
+            // 获取到目标文件夹
+            data = this.getTargetBookMarksFolder(data, words);
+            // 递归获取目标文件夹的所有书签
+            let list = this.getBookmarks(data);
 
-// 书签栏 移动设备书签 其它书签
-function analyseBookMark(data) {
-    let value = $('.ipt').val().trim();
-    var words = splitBookMarkName(value);
-    data = getFirstLevelBookMarksFloder(data);
-    // 获取到目标文件夹
-    data = getTargetBookMarksFloder(data, words);
-    // 递归获取目标文件夹的所有书签
-    let list = getBookmarks(data);
-    // console.log(arr);
-    getBookMarkDown(list);
-}
+            if (this.config.selected == "random") {
+                this.getRandomBookmark(list);
+            } else if (this.config.selected == "markdown") {
+                this.getMarkdown(list);
+            }
+        },
+        // 选择该文件夹
+        chooseFolder(folder) {
+            this.folderName = folder;
+        },
+        removeBookmarkByID(id) {
+            let flag = confirm("confrim remove this bookmark?")
+            if (flag) {
+                chrome.bookmarks.remove(id);
+                alert("remove sucess.");
+            }
+        },
+        // 获取 markdown 格式链接
+        getMarkdown(list) {
+            console.log("get markdown ...");
+            let str = "";
 
-// 获取 合法 rss 地址
-function getValidRSS(list) {
+            if (list.length == 0) {
+                str = "请参考下面输入样例 ：）";
+            } else {
+                str = '# List\n\n';
+                for (let i = 0; i < list.length; i++) {
+                    str = str.concat('[').concat(list[i].title).concat(']').concat('(').concat(list[i].url).concat(')\n\n');
+                }
 
-}
+            }
+            this.markdown.content = str;
+        },
+        // 随机获取一个书签
+        getRandomBookmark(list) {
+            if (list.length === 0) {
+                this.errorInfo = `
+                bookmark folder do not exists,
+                 try to check the root folder is '书签栏' or 'Bookmarks bar'.
+                `;
+            } else {
+                let index = Math.floor(Math.random() * list.length);
+                let bookmark = list[index];
+                this.randomBookmark.id = bookmark.id;
+                this.randomBookmark.url = bookmark.url;
+                this.randomBookmark.title = bookmark.title;
+                // window.open(this.url, 'target');
+            }
 
-// 获取 markdown 格式链接
-function getBookMarkDown(list) {
-    console.log("get markdown ...");
-    let str = "";
-
-    if (list.length == 0) {
-        str = "请参考下面输入样例 ：）";
-    } else {
-        str = '# List\n';
-        for (let i = 0; i < list.length; i++) {
-            str = str.concat('[').concat(list[i].title).concat(']').concat('(').concat(list[i].url).concat(')\n\n');
-        }
-
-    }
-    $('.mrk-cnt #mrk-cnt').val(str);
-}
-
-
-const rssConfig = ["/feed", "rss", "feed.xml", "atom.xml"]
-
-
-
-function getBookmarks(data) {
-    var list = [];
-    getBookmarksByFolder(data, list);
-    return list;
-}
-function getBookmarksByFolder(data, list) {
-    var recursionConfig = $('#recursion').is(':checked');
-
-    for (var children in data) {
-        if (typeof (data[children]) == 'object') {
-            let subData = data[children];
-            for (let index = 0; index < subData.length; index++) {
-                if (subData[index].url != undefined) {
-                    list.push({
-                        'title': subData[index].title,
-                        'url': subData[index].url
-                    })
-                } else {
-                    if (recursionConfig) {
-                        getBookmarksByFolder(subData[index], list);
+        },
+        splitBookMarkName(value) {
+            // value = "书签栏::博客"
+            var words = value.split('/')
+            return words;
+        },
+        getBookmarks(data) {
+            var list = [];
+            this.getBookmarksByFolder(data, list);
+            return list;
+        },
+        getBookmarksByFolder(data, list) {
+            var recursionConfig = this.recursionConfig;
+            for (var children in data) {
+                if (typeof (data[children]) == 'object') {
+                    let subData = data[children];
+                    for (let index = 0; index < subData.length; index++) {
+                        if (subData[index].url != undefined) {
+                            list.push({
+                                'id': subData[index].id,
+                                'title': subData[index].title,
+                                'url': subData[index].url
+                            })
+                        } else {
+                            if (recursionConfig) {
+                                this.getBookmarksByFolder(subData[index], list);
+                            }
+                        }
                     }
                 }
             }
-        }
-    }
-}
+        },
 
-function getTargetBookMarksFloder(data, words) {
-    if (words.length === 0) {
-        return data;
-    } else {
-        data = getTargetBookMarksFloderByWord(data, words[0]);
-        words.splice(0, 1);
-        return getTargetBookMarksFloder(data, words);
-    }
+        getTargetBookMarksFolder(data, words) {
+            if (words.length === 0) {
+                return data;
+            } else {
+                data = this.getTargetBookMarksFolderByWord(data, words[0]);
+                words.splice(0, 1);
+                return this.getTargetBookMarksFolder(data, words);
+            }
 
-}
+        },
 
-function getTargetBookMarksFloderByWord(data, word) {
-    for (let children in data) {
-        if (typeof (data[children]) == 'object') {
-            data = data[children];
-            for (let index in data) {
-                if (data[index].title === word) {
-                    return data[index];
+        getTargetBookMarksFolderByWord(data, word) {
+            for (let children in data) {
+                if (typeof (data[children]) == 'object') {
+                    data = data[children];
+                    for (let index in data) {
+                        if (data[index].title === word) {
+                            return data[index];
+                        }
+                    }
                 }
             }
-        }
-    }
-}
+        },
 
-// util function
-function getFirstLevelBookMarksFloder(data) {
-    for (var children in data) {
-        if (typeof (data[children]) == 'object' && data[children].title == '') {
-            return data[children];
-        } else {
-            return getFirstLevelBookMarksFloder(data[children]);
+        // util function 先获取根书签文件夹
+        getFirstLevelBookMarksFolder(data) {
+            for (var children in data) {
+                if (typeof (data[children]) == 'object' && data[children].title == '') {
+                    return data[children];
+                } else {
+                    return getFirstLevelBookMarksFolder(data[children]);
+                }
+            }
+        },
+        initConfig(data) {
+            data = this.getFirstLevelBookMarksFolder(data);
+            data.children.forEach(element => {
+                this.folders.push(element.title);
+            });
+            this.folderName = this.folders[0];
         }
-    }
-}
+    },
+
+})
